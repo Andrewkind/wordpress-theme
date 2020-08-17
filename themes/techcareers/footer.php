e12a1ed10e6b42b32f37fdde732e3ae0af50df88
Andrew Kind <andrew.w.kind@gmail.com>
2020-08-17 13:28:41 -0600

    Enqueue the theme stylesheet.

--- themes/techcareers/footer.php
+++ themes/techcareers/footer.php
@@ -1,20 +1,15 @@
-<footer>
-
-    <h2><?php bloginfo('title'); ?></h2>
+  <footer>
+    <h2><?php bloginfo( 'title' ); ?> Footer</h2>
     <p>
-        &copy; Copyright
-        <?php echo date('Y'); ?>
-        <a href="<?php echo site_url(); ?>"></a>
-        All Rights Reserved.
+      &copy; Copyright
+      <?php echo date( 'Y' ); ?>
+      <a href="<?php echo site_url(); ?>"><?php bloginfo( 'title' ); ?></a>;
+      All Rights Reserved.
     </p>
-    </body>
-
-</footer>
-
-<?php
-wp_footer();
-?>
-
+  </footer>
+  <?php
+    // Very similar to wp_head(), but for footer scripts or outputs.
+    wp_footer();
+  ?>
 </body>
-
 </html>
--- themes/techcareers/index.php
+++ themes/techcareers/index.php
@@ -1,14 +1,14 @@
 <?php
-
-get_header();
-
-
+  /**
+   * The get_header() function will grab and run our 'header.php' file for this theme.
+   */
+  get_header();
 ?>
-<p>Hello World</p>
+
+<p>This is index.php</p>
 
 <?php
-
-get_footer();
-
-
-?>
+  /**
+   * The get_footer() function will grab and run our 'footer.php' file for this theme.
+   */
+  get_footer();
